f8f27552994105ad7afa4b526240f5892f42f1f1
clirimtarcitaku <clirimtar.citaku@student.uni-pr.edu>
2024-04-18 15:59:17 +0200

    Navigation Bar

---
+++ ClientSide/profile_page.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../css/ClientSide/Home.css ">
+    <link rel="stylesheet" href="Home.css">
+    <script src="https://kit.fontawesome.com/a710a530b9.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="../assets/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <script defer src="../assets/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+
+    <title>Spring Hotel &amp; Spa</title>
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+</head>
+<body>
+    <div class="logoja">
+        <a href="index.php"><img src="../assets/images/Logo.png" alt="Logo" /></a>
+    </div>
+    
+    <nav class="navbar">
+        <div></div>
+        <ul>
+        <li><a href="./index.php">Home</a></li>
+            <li><a href="./About-Us.html">About Us</a></li>
+            <li><a href="./Contacts.html">Contacts</a></li>
+            <li><a href="./FAQ.html">FAQ</a></li>
+            <li><a href="./Events.php">Events</a></li>
+        </ul>
+
+        <a id="loginLink" href="profile_page.php">
+        <i class="fa-solid fa-user"></i>
+        </a>
+        <h7 id="welcomeMessage" style="display: none;">Welcome <?php echo $username; ?></h7>
+        <script>
+       
+            document.addEventListener("DOMContentLoaded", function() {
+        
+                const loginIcon = document.getElementById("loginIcon");
+                const welcomeMessage = document.getElementById("welcomeMessage");
+
+      
+                function toggleElements() {
+                loginIcon.style.display = (loginIcon.style.display === "none") ? "block" : "none";
+                welcomeMessage.style.display = (welcomeMessage.style.display === "none") ? "block" : "none";
+                }
+                toggleElements(); 
+
+        
+                setTimeout(function() {
+                setTimeout(toggleElements, 2000);
+                }, 2000); 
+            });
+        </script>
+    </nav>
+</body>
