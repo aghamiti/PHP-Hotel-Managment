586b995d2a59ef330b1ba5d56953bfd1074e1d73
Art Jashari <art.jashari1@student.uni-pr.edu>
2024-04-19 02:13:27 +0200

    Minor changes

--- AdminDash/Messages.php
+++ AdminDash/Messages.php
@@ -8,6 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon">
     <title>Spring Hotel | Messages | Admin</title>
     <link rel="stylesheet" href="../css/AdminDash/Messages.css" >
     <!-- Bootstrap CSS -->
