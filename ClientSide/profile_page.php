87bd97117934ef718984d5c09feaf1768549841c
clirimtarcitaku <clirimtar.citaku@student.uni-pr.edu>
2024-04-20 01:21:47 +0200

    Establishment of the database connection for profile_page.php

--- ClientSide/profile_page.php
+++ ClientSide/profile_page.php
@@ -1,19 +1,12 @@
 <?php
     session_start();
-        $user_id = $_SESSION['user_id'];
-        $dbserver = "";
-        $dbuser = "";
-        $dbpassword = "";
-        $dbname = "";
-        $conn = mysqli_connect($dbserver, $dbuser, $dbpassword, $dbname);
-        if (!$conn) {
-            echo 'Connection error: ' . mysqli_connect_error();
-        }
+        include_once '../API/db_connection.php';//Including the connection to the database
+
         $user_id = mysqli_real_escape_string($conn, $user_id);
         $sql = "SELECT * FROM Bookings";
         $sql1 = "SELECT Price FROM Rooms";
-        $result = mysqli_query($conn, $sql); //Rezultati i Bookings
-        $result1 = mysqli_query($conn, $sql1); //Prices ne Rooms
+        $result = mysqli_query($conn, $sql); 
+        $result1 = mysqli_query($conn, $sql1); 
         $bookings = mysqli_fetch_all($result, MYSQLI_ASSOC);
         $prices = mysqli_fetch_all($result1, MYSQLI_ASSOC);
         mysqli_free_result($result);
